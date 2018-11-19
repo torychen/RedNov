cc99240cd5f2f9e1652f09c32df763d3406a9522
admin <3903927@qq.com>
2018-11-19 17:49:09 +0800

    Change ui to green icon. ok.

--- code/RedNov/app/src/main/java/com/tory/rednov/view/VideoActivity.java
+++ code/RedNov/app/src/main/java/com/tory/rednov/view/VideoActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import com.tory.rednov.R;
@@ -39,6 +40,8 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
 
     private Uri videoUri;
 
+    ImageView ivPlay;
+
     private static final boolean DEBUG_INPUT_IP = false;
 
     @Override
@@ -62,15 +65,18 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
         }
 
 
-        SeekBar seekBar = findViewById(R.id.sbVideoProgress);
+        SeekBar seekBar = findViewById(R.id.sbTime);
         seekBar.setMax(100);
         seekBar.setProgress(1);
 
-        Button btnPlay = findViewById(R.id.btnPlayVideo);
-        btnPlay.setOnClickListener(this);
+        ivPlay = findViewById(R.id.ivPlay);
+        ivPlay.setOnClickListener(this);
 
-        Button btnPlayFile = findViewById(R.id.btnPlayFile);
-        btnPlayFile.setOnClickListener(this);
+        ImageView ivStop = findViewById(R.id.ivStop);
+        ivStop.setOnClickListener(this);
+
+        ImageView ivNext = findViewById(R.id.ivNext);
+        ivNext.setOnClickListener(this);
 
         svVideoMain  = findViewById(R.id.svVideoMain);
         LibVLC libVLC;
@@ -127,43 +133,28 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
         return uri;
     }
 
+    //TODO the player logic should be implemented.
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btnPlayVideo:
+            case R.id.ivNext:
+                Log.d(TAG, "onClick: ivFullScreen");
+                break;
 
-            case R.id.btnPlayFile:
+            case R.id.ivStop:
+                Log.d(TAG, "onClick: ivStop");
+                break;
+
+            case R.id.ivPlay:
                 if (!isPlaying) {
-
-                    ///storage/emulated/0/ is phone memory
-                    File[] files;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                        files = getExternalFilesDirs(Environment.MEDIA_MOUNTED);
-                        for (File file : files) {
-                            Log.d(TAG, "current storage path have:" + file.getAbsolutePath());
-                        }
-                    }
-
-                    File file = new File(Environment.getExternalStorageDirectory(), "/test.mp4");
-                    if (file.exists()) {
-                        //videoView.setVideoPath(file.getPath()); // 指定视频文件的路径
-                        Log.d(TAG, "sd path is " + file.getPath());
-
-                    } else {
-                        UtiToast.Toast("file NOT found. please check.");
-                        return;
-                    }
-
                     isPlaying = true;
+                    ivPlay.setImageResource(R.drawable.player_pause);
 
-                    //videoView.requestFocus();
-                    //videoView.start();
                     Log.d(TAG, "onClick: play");
                 } else {
-                    //TODO Should pause playing.
+                    isPlaying = false;
                     Log.d(TAG, "should pause video.");
-
-
+                    ivPlay.setImageResource(R.drawable.player_play);
                 }
 
                 break;
